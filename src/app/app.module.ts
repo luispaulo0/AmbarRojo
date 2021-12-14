f7dfd94013f32eee10986295c8b346407affc051
luispaulo0 <193279@ids.upchiapas.edu.mx>
2021-12-14 00:49:39 -0600

    Primer Commit Vista Homes casi terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import {Routes, RouterModule} from "@angular/router";
+import { CardsComponent } from './components/cards/cards.component';
+import { CardNewsComponent } from './components/card-news/card-news.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+
+];
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CardsComponent,
+    CardNewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
