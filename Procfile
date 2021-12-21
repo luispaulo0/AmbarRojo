7d3dab1f10a7a2409a33d753cf75a52dedd2b8ea
luispaulo0 <193279@ids.upchiapas.edu.mx>
2021-12-20 18:09:05 -0600

    arreglos entre otros

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:node src/karma.conf.js
--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
+    port: process.env.PORT || 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
